1d5cd6673a642e40cda126b2069b3b2b76df4c2d
Kchugames <85512384+Kchugames@users.noreply.github.com>
2021-07-26 17:05:21 +0100

    Add files via upload
    Added to Main

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Timer from './TimerApp/Timer';
+import ToDoAppHome from './ToDoApp/ToDoAppHome.js';
+
+function App() {
+  return (
+    <div className="App">
+    
+      <Timer/>
+
+
+      <ToDoAppHome/>
+    </div>
+  );
+}
+
+export default App; <ToDoAppHome/>
